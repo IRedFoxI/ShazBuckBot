237baab7275b84a24fdb4fc6a5fad6ccbcbf3b06
|RedFox| <redfox@club77.org>
2020-12-13 16:13:19 +0800

    Add clean_picking.py utility to easily remove stale Picking games from the database

---
+++ clean_picking.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""A discord bot to bet on PUGs."""
+
+from enum import IntEnum
+import yaml
+import sqlite3
+
+from typing import List, Tuple
+
+config = yaml.safe_load(open("config.yml"))
+DATABASE = config['database']
+GAME_STATUS = IntEnum('Game_Status', 'Picking Cancelled InProgress Team1 Team2 Tied')
+WAGER_RESULT = IntEnum('Wager_Result', 'InProgress Won Lost Canceled')
+
+# Main
+if __name__ == '__main__':
+    conn = sqlite3.connect(DATABASE)
+    sql = ''' DELETE FROM games WHERE status = ? '''
+    values = (GAME_STATUS.Picking,)
+    cur = conn.cursor()
+    cur.execute(sql, values)
+    conn.commit()
+    conn.close()
+
+

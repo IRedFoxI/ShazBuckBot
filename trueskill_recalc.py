40ed036d4d4aa522ac2920f13b9ad05553ec7669
|RedFox| <redfox@club77.org>
2021-08-16 11:53:02 +0800

    Update trueskill_recalc.py to ignore games which are not 5v5

--- trueskill_recalc.py
+++ trueskill_recalc.py
@@ -1,13 +1,22 @@
 # -*- coding: utf-8 -*-
 """Calculate TrueSkill of players."""
 
-from enum import IntEnum
+from enum import IntEnum, auto
 import yaml
 import sqlite3
-from trueskill import Rating, rate, quality, backends, BETA
+from trueskill import Rating, rate, quality, backends
 
 FIRST_GAME_ID = 495
-GAME_STATUS = IntEnum('Game_Status', 'Picking Cancelled InProgress Team1 Team2 Tied')
+
+
+class GameStatus(IntEnum):
+    PICKING = auto()
+    CANCELLED = auto()
+    INPROGRESS = auto()
+    TEAM1 = auto()
+    TEAM2 = auto()
+    TIED = auto()
+
 
 config = yaml.safe_load(open("config.yml"))
 DATABASE = config['database']
@@ -35,7 +44,7 @@ conn.execute("""
 player_ratings = {}
 number_of_games = {}
 backends.choose_backend('scipy')
-values = (FIRST_GAME_ID, GAME_STATUS.Team1, GAME_STATUS.Team2, GAME_STATUS.Tied)
+values = (FIRST_GAME_ID, GameStatus.TEAM1, GameStatus.TEAM2, GameStatus.TIED)
 sql = ''' SELECT id, team1, team2, status 
           FROM games 
           WHERE id > ? 
@@ -49,40 +58,41 @@ for game in games:
     team1_str: str = game[1]
     team2_str: str = game[2]
     status: int = game[3]
-    team1_skills = []
-    for player in team1_str.split():
-        if player not in player_ratings:
-            player_ratings[player] = Rating()
-        team1_skills.append(player_ratings[player])
-    team2_skills = []
-    for player in team2_str.split():
-        if player not in player_ratings:
-            player_ratings[player] = Rating()
-        team2_skills.append(player_ratings[player])
-    ranks = [0, 0]
-    if status == GAME_STATUS.Team1:
-        ranks = [0, 1]
-    elif status == GAME_STATUS.Team2:
-        ranks = [1, 0]
-    draw_chance = quality([team1_skills, team2_skills])
-    print(f'id: {game_id}, chance to draw: {draw_chance:.2f}, result: {GAME_STATUS(status).name}.')
-    (new_team1_skills, new_team2_skills) = rate([team1_skills, team2_skills], ranks)
-    for idx, player in enumerate(team1_str.split()):
-        rating = new_team1_skills[idx]
-        player_ratings[player] = rating
-        trueskill_update = (player, game_id, rating.mu, rating.sigma, rating.exposure)
-        sql = ''' INSERT INTO trueskills(discord_id, game_id, mu, sigma, trueskill)
-                  VALUES(?, ?, ?, ?, ?) '''
-        cur = conn.cursor()
-        cur.execute(sql, trueskill_update)
-    for idx, player in enumerate(team2_str.split()):
-        rating = new_team2_skills[idx]
-        player_ratings[player] = rating
-        trueskill_update = (player, game_id, rating.mu, rating.sigma, rating.exposure)
-        sql = ''' INSERT INTO trueskills(discord_id, game_id, mu, sigma, trueskill)
-                  VALUES(?, ?, ?, ?, ?) '''
-        cur = conn.cursor()
-        cur.execute(sql, trueskill_update)
+    if len(team1_str.split()) == 5 or len(team2_str.split()) == 5:
+        team1_skills = []
+        for player in team1_str.split():
+            if player not in player_ratings:
+                player_ratings[player] = Rating()
+            team1_skills.append(player_ratings[player])
+        team2_skills = []
+        for player in team2_str.split():
+            if player not in player_ratings:
+                player_ratings[player] = Rating()
+            team2_skills.append(player_ratings[player])
+        ranks = [0, 0]
+        if status == GameStatus.TEAM1:
+            ranks = [0, 1]
+        elif status == GameStatus.TEAM2:
+            ranks = [1, 0]
+        draw_chance = quality([team1_skills, team2_skills])
+        print(f'id: {game_id}, chance to draw: {draw_chance:.2f}, result: {GameStatus(status).name}.')
+        (new_team1_skills, new_team2_skills) = rate([team1_skills, team2_skills], ranks)
+        for idx, player in enumerate(team1_str.split()):
+            rating = new_team1_skills[idx]
+            player_ratings[player] = rating
+            trueskill_update = (player, game_id, rating.mu, rating.sigma, rating.exposure)
+            sql = ''' INSERT INTO trueskills(discord_id, game_id, mu, sigma, trueskill)
+                      VALUES(?, ?, ?, ?, ?) '''
+            cur = conn.cursor()
+            cur.execute(sql, trueskill_update)
+        for idx, player in enumerate(team2_str.split()):
+            rating = new_team2_skills[idx]
+            player_ratings[player] = rating
+            trueskill_update = (player, game_id, rating.mu, rating.sigma, rating.exposure)
+            sql = ''' INSERT INTO trueskills(discord_id, game_id, mu, sigma, trueskill)
+                      VALUES(?, ?, ?, ?, ?) '''
+            cur = conn.cursor()
+            cur.execute(sql, trueskill_update)
 for player in player_ratings.keys():
     player_nick = player
     sql = ''' SELECT nick FROM users WHERE discord_id = ? '''
